434c4b841b8368597d685be0722a34efe4f8f408
ravipatel7064 <ravi.patel7064@gmail.com>
2025-12-28 15:10:46 +0530

    initial Commit

---
+++ assignment 1 python.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print("\n")
+print(f"addition : {addition}")
+print(f"subtraction : {subtraction}")
+print(f"multiplication : {multiplication}")
+print(f"division : {division}")
+
+
